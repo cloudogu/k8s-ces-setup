913735d771beac83080e8f1ea00c860cb6a9613f
Philipp Pixel <boris.schaa@cloudogu.com>
2022-03-22 13:43:03 +0100

    #4 Refactor to kubectl including (does not work)

--- app/core/apply.go
+++ app/core/apply.go
@@ -1,80 +1,50 @@
 package core
 
 import (
-	"context"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/discovery/cached/memory"
-	"k8s.io/client-go/dynamic"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/restmapper"
+	"k8s.io/kubectl/pkg/cmd/apply"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"os"
+	"time"
 )
 
 type k8sApplyClient struct {
-	clusterConfig *rest.Config
+	clusterConfig  *rest.Config
+	kubectlFactory cmdutil.Factory
+	applyIOStreams genericclioptions.IOStreams
 }
 
 // NewK8sClient creates a `kubectl`-like client which operates on the K8s API.
 func NewK8sClient(clusterConfig *rest.Config) *k8sApplyClient {
-	return &k8sApplyClient{clusterConfig: clusterConfig}
+
+	kubectlDefaultConfigFlags := genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag().WithDiscoveryBurst(300).WithDiscoveryQPS(50.0)
+	flagsFile := pflag.NewFlagSet("f", pflag.ExitOnError)
+	err := flagsFile.Parse([]string{"-f", "-"})
+	if err != nil {
+		logrus.Error("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		logrus.Error(err.Error())
+		time.Sleep(60 * time.Second)
+		panic("oh no, not again")
+	}
+	kubectlDefaultConfigFlags.AddFlags(flagsFile)
+	matchVersionKubeConfigFlags := cmdutil.NewMatchVersionFlags(kubectlDefaultConfigFlags)
+	kubectlFactory := cmdutil.NewFactory(matchVersionKubeConfigFlags)
+	applyIO := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+
+	return &k8sApplyClient{
+		clusterConfig:  clusterConfig,
+		kubectlFactory: kubectlFactory,
+		applyIOStreams: applyIO,
+	}
 }
 
 // Apply sends a request to the K8s API with the provided YAML resources in order to apply them to the current cluster.
 func (dkc *k8sApplyClient) Apply(yamlResources []byte) error {
-	var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-	ctx := context.Background()
 
-	// 1. Prepare a RESTMapper to find GVR
-	dc, err := discovery.NewDiscoveryClientForConfig(dkc.clusterConfig)
-	if err != nil {
-		return err
-	}
-	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+	cmdApply := apply.NewCmdApply("kubectl", dkc.kubectlFactory, dkc.applyIOStreams)
 
-	// 2. Prepare the dynamic client
-	dyn, err := dynamic.NewForConfig(dkc.clusterConfig)
-	if err != nil {
-		return err
-	}
-
-	// 3. Decode YAML manifest into unstructured.Unstructured
-	k8sObjects := &unstructured.Unstructured{}
-	_, gvk, err := decUnstructured.Decode(yamlResources, nil, k8sObjects)
-	if err != nil {
-		return err
-	}
-
-	// 4. Find GVR
-	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
-	if err != nil {
-		return err
-	}
-
-	// 5. Obtain REST interface for the GVR
-	var dr dynamic.ResourceInterface
-	if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
-		// namespaced resources should specify the namespace
-		dr = dyn.Resource(mapping.Resource).Namespace(k8sObjects.GetNamespace())
-	} else {
-		// for cluster-wide resources
-		dr = dyn.Resource(mapping.Resource)
-	}
-
-	// 6. Marshal object into JSON
-	//data, err := json.Marshal(k8sObjects)
-	//if err != nil {
-	//	return err
-	//}
-
-	// 7. Create or Update the object with SSA
-	//     types.ApplyPatchType indicates SSA.
-	//     FieldManager specifies the field owner ID.
-	_, err = dr.Create(ctx, k8sObjects, metav1.CreateOptions{
-		FieldManager: "sample-controller",
-	})
-
-	return err
+	return cmdApply.Execute()
 }
