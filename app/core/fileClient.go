fcb9bc99a4f4a6a1d8c330160f9b4f66927c6467
Philipp Pixel <boris.schaa@cloudogu.com>
2022-03-22 15:17:36 +0100

    #4 Add user agent to identify http client calls
    The go http client uses the curllib and thus the user-agent "curl/...". This
    commit adds a user-agent to distinguish http problems towards the YAML resources
    from HTTP problems towards the K8s API.
    
    Also the user agent includes the app version which may produce some light-weight
    statistics.

--- app/core/fileClient.go
+++ app/core/fileClient.go
@@ -2,23 +2,35 @@ package core
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 )
 
 type defaultHttpClient struct {
 	httpClient *http.Client
+	version    string
 }
 
 func NewFileClient(appVersion string) *defaultHttpClient {
 	return &defaultHttpClient{
 		httpClient: &http.Client{},
+		version:    appVersion,
 	}
 }
 
 // Get retrieves a file over HTTP returns its contents.
 func (dhc *defaultHttpClient) Get(url string) ([]byte, error) {
-	resp, err := dhc.httpClient.Get(url)
+	logrus.Debugf("Getting resource from %s", url)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Cloudia/"+dhc.version)
+
+	resp, err := dhc.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
