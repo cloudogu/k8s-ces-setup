7e43a6ee203e11a170e8b4695c717d7c3dedf454
Philipp Pixel <boris.schaa@cloudogu.com>
2022-04-01 11:11:54 +0200

    #4 Rename secrets independent of authentication source
    The secret names must not be too generic though in order to avoid accidental
    overwriting/usage from user-custom secrets within that namespace.

--- app/setup/secretCreationStep.go
+++ app/setup/secretCreationStep.go
@@ -9,23 +9,23 @@ import (
 )
 
 const (
-	secretNameDoguRegistry   = "dogu-cloudogu-com"
-	secretNameDockerRegistry = "registry-cloudogu-com"
+	secretNameDoguRegistry  = "cloudogu-dogu-registry"
+	secretNameImageRegistry = "cloudogu-image-registry"
 )
 
 // secretCreator contains necessary information to create a new secrets in the cluster.
 type secretCreator struct {
-	ClientSet                 kubernetes.Interface `json:"client_set"`
-	TargetNamespace           string               `json:"target_namespace"`
-	CredentialSourceNamespace string               `yaml:"credential_source_namespace"`
+	clientSet                 kubernetes.Interface
+	targetNamespace           string
+	credentialSourceNamespace string
 }
 
 // newSecretCreator creates a new object of type secretCreator.
 func newSecretCreator(clientSet kubernetes.Interface, targetNamespace, credentialSourceNamespace string) *secretCreator {
 	newProvisioner := &secretCreator{
-		ClientSet:                 clientSet,
-		TargetNamespace:           targetNamespace,
-		CredentialSourceNamespace: credentialSourceNamespace,
+		clientSet:                 clientSet,
+		targetNamespace:           targetNamespace,
+		credentialSourceNamespace: credentialSourceNamespace,
 	}
 	return newProvisioner
 }
@@ -35,7 +35,7 @@ func (sc *secretCreator) createSecrets() error {
 	if err != nil {
 		return fmt.Errorf("failed to copy dogu registry secret: %w", err)
 	}
-	err = sc.copySecretToTargetNamespace(secretNameDockerRegistry)
+	err = sc.copySecretToTargetNamespace(secretNameImageRegistry)
 	if err != nil {
 		return fmt.Errorf("failed to copy docker image pull secret: %w", err)
 	}
@@ -45,7 +45,7 @@ func (sc *secretCreator) createSecrets() error {
 
 // copySecretToTargetNamespace copies a secret from one namespace to another by rewriting the namespace.
 func (sc *secretCreator) copySecretToTargetNamespace(secretName string) error {
-	setupSecretInterface := sc.ClientSet.CoreV1().Secrets(sc.CredentialSourceNamespace)
+	setupSecretInterface := sc.clientSet.CoreV1().Secrets(sc.credentialSourceNamespace)
 
 	originalSecret, err := setupSecretInterface.Get(context.Background(), secretName, metav1.GetOptions{})
 	if err != nil {
@@ -54,13 +54,13 @@ func (sc *secretCreator) copySecretToTargetNamespace(secretName string) error {
 
 	secretCopy := &v1.Secret{
 		TypeMeta:   originalSecret.TypeMeta,
-		ObjectMeta: metav1.ObjectMeta{Name: secretName, Namespace: sc.TargetNamespace},
+		ObjectMeta: metav1.ObjectMeta{Name: secretName, Namespace: sc.targetNamespace},
 		Immutable:  originalSecret.Immutable,
 		Data:       originalSecret.Data,
 		StringData: originalSecret.StringData,
 		Type:       originalSecret.Type,
 	}
-	_, err = sc.ClientSet.CoreV1().Secrets(sc.TargetNamespace).Create(context.Background(), secretCopy, metav1.CreateOptions{})
+	_, err = sc.clientSet.CoreV1().Secrets(sc.targetNamespace).Create(context.Background(), secretCopy, metav1.CreateOptions{})
 	if err != nil {
 		return err
 	}
@@ -70,7 +70,7 @@ func (sc *secretCreator) copySecretToTargetNamespace(secretName string) error {
 
 // GetStepDescription returns the description of the namespace creation step.
 func (sc *secretCreator) GetStepDescription() string {
-	return fmt.Sprintf("Create new Secrets in namespace %s", sc.TargetNamespace)
+	return fmt.Sprintf("Create new Secrets in namespace %s", sc.targetNamespace)
 }
 
 // PerformSetupStep creates a namespace during setup execution.
