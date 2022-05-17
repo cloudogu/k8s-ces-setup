16eed0cb7ceb4ee5faccfdfe67ca942daec0b713
Niklas <niklas.hinze@cloudogu.com>
2022-05-17 14:40:22 +0200

    Reorder data steps, update dogu operator url #12

--- app/setup/executor.go
+++ app/setup/executor.go
@@ -145,14 +145,14 @@ func (e *Executor) RegisterDataSetupSteps(etcdRegistry registry.Registry) error 
 	configWriter := data.NewRegistryConfigurationWriter(etcdRegistry)
 
 	// register steps
+	e.RegisterSetupStep(data.NewKeyProviderStep(configWriter, e.SetupContext.AppConfig.KeyProvider))
 	e.RegisterSetupStep(data.NewInstanceSecretValidatorStep(e.ClientSet, e.SetupContext.AppConfig.TargetNamespace))
 	e.RegisterSetupStep(data.NewWriteAdminDataStep(configWriter, &e.SetupContext.StartupConfiguration))
 	e.RegisterSetupStep(data.NewWriteNamingDataStep(configWriter, &e.SetupContext.StartupConfiguration))
-	e.RegisterSetupStep(data.NewWriteDoguDataStep(configWriter, &e.SetupContext.StartupConfiguration))
+	e.RegisterSetupStep(data.NewWriteRegistryConfigEncryptedStep(&e.SetupContext.StartupConfiguration, e.ClientSet, e.SetupContext.AppConfig.TargetNamespace))
 	e.RegisterSetupStep(data.NewWriteLdapDataStep(configWriter, &e.SetupContext.StartupConfiguration))
 	e.RegisterSetupStep(data.NewWriteRegistryConfigDataStep(configWriter, &e.SetupContext.StartupConfiguration))
-	e.RegisterSetupStep(data.NewKeyProviderStep(configWriter, e.SetupContext.AppConfig.KeyProvider))
-	e.RegisterSetupStep(data.NewWriteRegistryConfigEncryptedStep(&e.SetupContext.StartupConfiguration, e.ClientSet, e.SetupContext.AppConfig.TargetNamespace))
+	e.RegisterSetupStep(data.NewWriteDoguDataStep(configWriter, &e.SetupContext.StartupConfiguration))
 
 	return nil
 }
