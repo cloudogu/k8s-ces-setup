10a48a36d0610afd5983bac0e6961f58d9c3b450
Jeremias Weber <jeremias.weber@cloudogu.com>
2022-12-05 14:42:22 +0100

    #30 Document etcd registry creation

--- app/cesregistry/etcd.go
+++ app/cesregistry/etcd.go
@@ -2,11 +2,13 @@ package cesregistry
 
 import (
 	"fmt"
+
 	"github.com/cloudogu/cesapp-lib/core"
 	"github.com/cloudogu/cesapp-lib/registry"
 	"github.com/cloudogu/k8s-ces-setup/app/setup/component"
 )
 
+// CreateEtcd returns a new etcd registry pointing to the specified namespace.
 func CreateEtcd(namespace string) (registry.Registry, error) {
 	return registry.New(core.Registry{
 		Type:      "etcd",
