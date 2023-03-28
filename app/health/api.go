97f795814c0f78cce992c05c5d105e7420c2faa5
Niklas <niklas.hinze@cloudogu.com>
2023-03-28 07:53:12 +0200

    #44 Ignore request logs for the health endpoint because they spammed the log output.

--- app/health/api.go
+++ app/health/api.go
@@ -7,11 +7,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const endpointGetHealth = "/api/v1/health"
+const EndpointGetHealth = "/api/v1/health"
 
 // SetupAPI setups the REST API for configuration information
 func SetupAPI(router gin.IRoutes, version string) {
 	requestHandler := newRequestHandler(version)
-	logrus.Debugf("Register endpoint [%s][%s]", http.MethodGet, endpointGetHealth)
-	router.GET(endpointGetHealth, requestHandler.getHealth)
+	logrus.Debugf("Register endpoint [%s][%s]", http.MethodGet, EndpointGetHealth)
+	router.GET(EndpointGetHealth, requestHandler.getHealth)
 }
