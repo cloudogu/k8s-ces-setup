0c0d050fbd90eacf148511496b42825d607093c4
Philipp Pixel <boris.schaa@cloudogu.com>
2023-08-11 13:17:41 +0200

    #56 Test deletion and re-creation of load-balancer

--- app/setup/data/createLoadBalancerStep_test.go
+++ app/setup/data/createLoadBalancerStep_test.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"context"
 	appctx "github.com/cloudogu/k8s-ces-setup/app/context"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -9,15 +11,12 @@ import (
 	"testing"
 )
 
+var testCtx = context.Background()
+
 func Test_createLoadBalancerStep_PerformSetupStep(t *testing.T) {
 	t.Run("creates load-balancer if none exists", func(t *testing.T) {
 		// given
-		serviceResource := &corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: cesLoadbalancerName,
-			},
-		}
-		fakeClient := fake.NewSimpleClientset(serviceResource)
+		fakeClient := fake.NewSimpleClientset()
 		config := &appctx.SetupJsonConfiguration{Naming: appctx.Naming{Fqdn: ""}, Dogus: appctx.Dogus{Install: []string{nginxIngressName}}}
 		sut := NewCreateLoadBalancerStep(config, fakeClient, testNamespace)
 
@@ -25,14 +24,22 @@ func Test_createLoadBalancerStep_PerformSetupStep(t *testing.T) {
 		err := sut.PerformSetupStep()
 
 		// then
-		require.ErrorContains(t, err, `services "ces-loadbalancer" already exists`)
+		require.NoError(t, err)
+		actual, err := fakeClient.CoreV1().Services(testNamespace).Get(testCtx, "ces-loadbalancer", metav1.GetOptions{})
+		require.NoError(t, err)
+		assert.NotNil(t, actual)
+		expected := map[string]string{"app": "ces"}
+		assert.Equal(t, expected, actual.ObjectMeta.Labels)
 	})
-	t.Run("deletes existing load-balancer and creates a new one", func(t *testing.T) {
+	t.Run("deletes misconfigured but still existing service and creates a new one", func(t *testing.T) {
 		// given
 		serviceResource := &corev1.Service{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: cesLoadbalancerName,
+
+				Name:   cesLoadbalancerName,
+				Labels: map[string]string{"delete": "me"},
 			},
+			Spec: corev1.ServiceSpec{Type: corev1.ServiceTypeClusterIP},
 		}
 		fakeClient := fake.NewSimpleClientset(serviceResource)
 		config := &appctx.SetupJsonConfiguration{Naming: appctx.Naming{Fqdn: ""}, Dogus: appctx.Dogus{Install: []string{nginxIngressName}}}
@@ -42,6 +49,12 @@ func Test_createLoadBalancerStep_PerformSetupStep(t *testing.T) {
 		err := sut.PerformSetupStep()
 
 		// then
-		require.ErrorContains(t, err, `services "ces-loadbalancer" already exists`)
+		require.NoError(t, err)
+		actual, err := fakeClient.CoreV1().Services(testNamespace).Get(testCtx, "ces-loadbalancer", metav1.GetOptions{})
+		require.NoError(t, err)
+		assert.NotNil(t, actual)
+		expected := map[string]string{"app": "ces"}
+		assert.Equal(t, expected, actual.ObjectMeta.Labels)
+		assert.Equal(t, corev1.ServiceTypeLoadBalancer, actual.Spec.Type)
 	})
 }
