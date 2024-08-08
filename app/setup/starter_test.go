56359c81f9205d239e14c7f7d455c8784b5ae76f
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2024-08-08 12:21:34 +0200

    #97 create config before components
    The k8s-dogu-oparator starts a watch the global-config on startup, but stopps with an error if this config does not exist. Therefore, the config should be created before creating the components (like k8s-dogu-operator)

--- app/setup/starter_test.go
+++ app/setup/starter_test.go
@@ -149,6 +149,7 @@ func TestStarter_StartSetup(t *testing.T) {
 		expect.RegisterLoadBalancerFQDNRetrieverSteps().Return(nil)
 		expect.RegisterSSLGenerationStep().Return(nil)
 		expect.RegisterValidationStep().Return(nil)
+		expect.RegisterDataSetupSteps(mock.Anything, mock.Anything).Return(nil)
 		expect.RegisterComponentSetupSteps().Return(assert.AnError)
 		starter.SetupExecutor = executorMock
 
@@ -167,7 +168,6 @@ func TestStarter_StartSetup(t *testing.T) {
 		expect.RegisterLoadBalancerFQDNRetrieverSteps().Return(nil)
 		expect.RegisterSSLGenerationStep().Return(nil)
 		expect.RegisterValidationStep().Return(nil)
-		expect.RegisterComponentSetupSteps().Return(nil)
 		expect.RegisterDataSetupSteps(mock.Anything, mock.Anything).Return(assert.AnError)
 		starter.SetupExecutor = executorMock
 
