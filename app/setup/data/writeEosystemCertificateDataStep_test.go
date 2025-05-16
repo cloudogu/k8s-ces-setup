b5d0cee06da348b861f4b1a41e2d69d45a801955
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-05-16 13:59:00 +0200

    #140 create new mocks

--- app/setup/data/writeEosystemCertificateDataStep_test.go
+++ app/setup/data/writeEosystemCertificateDataStep_test.go
@@ -16,7 +16,7 @@ func TestNewWriteEcosystemCertificateDataStep(t *testing.T) {
 	t.Run("successfully create new naming data step", func(t *testing.T) {
 		// given
 		testConfig := &appcontext.SetupJsonConfiguration{}
-		secretClient := NewMockSecretClient(t)
+		secretClient := newMockSecretClient(t)
 
 		// when
 		myStep := NewWriteEcosystemCertificateDataStep(secretClient, testConfig)
@@ -30,7 +30,7 @@ func Test_writeEcosystemCertificateDataStep_GetStepDescription(t *testing.T) {
 	t.Run("successfully get naming data step description", func(t *testing.T) {
 		// given
 		testConfig := &appcontext.SetupJsonConfiguration{}
-		secretClient := NewMockSecretClient(t)
+		secretClient := newMockSecretClient(t)
 
 		// when
 		myStep := NewWriteEcosystemCertificateDataStep(secretClient, testConfig)
@@ -59,7 +59,7 @@ func Test_writeEcosystemCertificateDataStep_PerformSetupStep(t *testing.T) {
 		}
 		// given
 		testConfig := &appcontext.SetupJsonConfiguration{}
-		secretClient := NewMockSecretClient(t)
+		secretClient := newMockSecretClient(t)
 		secretClient.EXPECT().Create(testCtx, certificateSecret, metav1.CreateOptions{}).Return(nil, assert.AnError)
 
 		myStep := NewWriteEcosystemCertificateDataStep(secretClient, testConfig)
@@ -83,7 +83,7 @@ func Test_writeEcosystemCertificateDataStep_PerformSetupStep(t *testing.T) {
 		}
 		// given
 		testConfig := &appcontext.SetupJsonConfiguration{}
-		secretClient := NewMockSecretClient(t)
+		secretClient := newMockSecretClient(t)
 		secretClient.EXPECT().Create(testCtx, certificateSecret, metav1.CreateOptions{}).Return(nil, nil)
 
 		myStep := NewWriteEcosystemCertificateDataStep(secretClient, testConfig)
